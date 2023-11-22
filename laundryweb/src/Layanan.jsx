b3b2a8589757446b0e2f9419e78edb02f756e4b4
PutriPrema <putriprema14@gmail.com>
2023-11-23 07:50:38 +0800

    update

--- laundryweb/src/Layanan.jsx
+++ laundryweb/src/Layanan.jsx
@@ -4,13 +4,16 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Button from 'react-bootstrap/Button';
 import Cards from './components/Cards'
+import SearchButton from './components/SearchButton'
 
 const Layanan = () => {
   const [text, setText] = useState('');
   return (
     <div>
       <Navbar />
-      ini layanan
+      <SearchButton/>
+      <h1 className="text-center" style={{ marginTop: "5%", color: 'black', fontSize: '24pt', fontWeight: 'bold' }}>Layanan Laundry yang Tersedia</h1>
+      <Cards/>
       <Footer />
     </div>
   );
