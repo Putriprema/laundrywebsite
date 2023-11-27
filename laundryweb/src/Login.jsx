defdf0c586c347a1698944259eff30a3afea4f97
ArifAfkar <putraasahi1@gmail.com>
2023-11-28 07:31:38 +0800

    progres login

--- laundryweb/src/Login.jsx
+++ laundryweb/src/Login.jsx
@@ -1,31 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from "./components/Navbar";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import vector from './assets/vector_login.png'
+import { Link } from 'react-router-dom';
 
-function BasicExample() {
+function LoginPelanggan() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          Well never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Navbar />
+      <div className='flex'>
+        <Form className='w-2/4'>
+          <Form.Text>Selamat Datang di Laundry Ease</Form.Text>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Kata Sandi</Form.Label>
+            <Form.Control type="password" placeholder="Kata Sandi" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Text>Lupa Kata Sandi?</Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Button variant="primary" type="submit">
+              Masuk
+            </Button>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Text>Belum Punya Akun? <Link to=""><span>Daftar</span></Link></Form.Text>
+          </Form.Group>
+        </Form>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '89.1vh', background: '#C4DAE8'}} >
+          <img src={vector} alt="" />
+        </div>
+      </div>
+    </>
   );
 }
 
-export default BasicExample;
+export default LoginPelanggan;
