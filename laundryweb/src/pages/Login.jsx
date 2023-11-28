cc0b5537d8fb938f4c7951d65b7625c280e3601b
ArifAfkar <putraasahi1@gmail.com>
2023-11-28 16:35:12 +0800

    login aman

--- laundryweb/src/pages/Login.jsx
+++ laundryweb/src/pages/Login.jsx
@@ -3,40 +3,47 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-// import vector from './assets/vector_login.png'
+import vector from '../assets/vector_login.png'
 import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 function LoginPelanggan() {
   return (
     <>
       <Navbar />
       <div className='flex'>
-        <Form className='w-2/4'>
-          <Form.Text>Selamat Datang di Laundry Ease</Form.Text>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form className='flex flex-col justify-center items-center w-1/2'>
+          <div className='flex justify-start w-3/4 mb-20'>
+            <h2>Selamat Datang di Laundry Ease</h2>
+          </div>
+          <div className='flex justify-start w-3/4 mb-10'>
+            <h3>Masuk</h3>
+          </div>
+          <Form.Group style={{marginBottom: '12px', width: '75%'}} controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Email" />
+            <Form.Control style={{border:'none', borderRadius: '10px', boxShadow: '1px 3px 4px grey'}} type="email" placeholder="Email" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mt-4 mb-2 w-3/4" controlId="formBasicPassword">
             <Form.Label>Kata Sandi</Form.Label>
-            <Form.Control type="password" placeholder="Kata Sandi" />
+            <Form.Control style={{border:'none', borderRadius: '10px', boxShadow: '1px 3px 4px grey'}} type="password" placeholder="Kata Sandi" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Text>Lupa Kata Sandi?</Form.Text>
+          <Form.Group className="flex justify-end mb-3 w-3/4" controlId="formBasicPassword">
+            <Link style={{textDecoration: 'none'}} to='/'><p style={{color: 'black'}}>Lupa Kata Sandi?</p></Link>
           </Form.Group>
           <Form.Group>
-            <Button variant="primary" type="submit">
+            <Button style={{padding: '8px 35px', borderRadius: '13px', boxShadow: '1px 3px 4px grey', marginBottom: '20px'}} variant="primary" type="submit">
               Masuk
             </Button>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Text>Belum Punya Akun? <Link to=""><span>Daftar</span></Link></Form.Text>
+            <p>Belum Punya Akun? <Link style={{textDecoration: 'none'}} to=""><span style={{color: 'black'}}>Daftar</span></Link></p>
           </Form.Group>
         </Form>
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '89.1vh', background: '#C4DAE8'}} >
-          {/* <img src={vector} alt="" /> */}
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '130vh', background: '#C4DAE8'}} >
+          <img src={vector} alt="image" />
         </div>
       </div>
+      <Footer />
     </>
   );
 }
